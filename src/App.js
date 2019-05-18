75b3bf4b7dd495d6fbb02e518051d0c84ce6c140
Somers Matthews <somersbmatthews@gmail.com>
2019-05-17 22:35:22 -0500

    imported from TweenMax for first time

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Text with fade in.</h1>
-      <h1>Text with wobble.</h1>
-      <h1>Text with fade in and wobble</h1>
-    </div>
-  );
+import Tween from 'gsap/TweenMax';
+
+class App() {
+  constructor(props) {
+    
+    this.myElement = null;
+    this.myTween = null;
+  }
+
+  componentDidMount() {
+  render() {
+    return (
+      <div className="App">
+        <h1>Text with fade in.</h1>
+        <h1>Text with wobble.</h1>
+        <h1>Text with fade in and wobble</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
