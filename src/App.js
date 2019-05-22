e17515d443e6d50795cdce86252ca6b185af4e53
Somers Matthews <somersbmatthews@gmail.com>
2019-05-22 14:14:11 -0500

    fixed wobble css animation in [App.js]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 
-import Tween from 'gsap/TweenMax';
+// const Tween = require('gsap/TweenMax');
 
-class App() {
-  constructor(props) {
+class App extends Component {
+  constructor() {
+    super()
     // this.myElement forms the bridge between the gsap library and the dom node
     // via React
     this.myElement = null;
@@ -16,14 +17,14 @@ class App() {
 
   componentDidMount(){
     // use the node ref to create the animation
-    this.myTween = Tween.to(this.myElement, 1, {x: 100, y: 100});
+    // this.myTween = Tween.to(this.myElement, 1, {x: 100, y: 100});
   }
 
     render() {
       return (
         <div className="App" ref={div => this.myElement = div}>
           <h1>Text with fade in.</h1>
-          <h1>Text with wobble.</h1>
+          <h1 className="animated wobble">Text with wobble.</h1>
           <h1>Text with fade in and wobble</h1>
         </div>
       );
