c00511d251f8e1814d4f9bef5be62cc8ae615503
Somers Matthews <somersbmatthews@gmail.com>
2019-05-31 21:44:20 -0500

    added variables for animation in constructor in [App.js]

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,9 @@ import React, {Component} from 'react';
 class Nav extends Component {
    constructor() {
       super()
-   
-      
+      this.element = null;
+
+      this.tween = null;
    }
 }
 
