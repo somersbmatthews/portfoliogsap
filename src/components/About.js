f27b5211a850ce17abf41d6d6641c36e1bbfc004
Somers Matthews <somersbmatthews@gmail.com>
2019-06-10 22:01:27 -0500

    created [About.js]

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+   constructor() {
+      super()
+      // this.element forms the bridge between the gsap library and the dom node
+      // via React
+      this.element = null;
+
+      // this.tween is the actual animation
+      this.tween = null;
+   }
+}
